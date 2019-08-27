9807ef3415bbf43559a22597b4cc8946ad7c7352
9MaxKuznetsov <jztenk@gmail.com>
2019-08-28 02:18:54 +0300

    onLoadMoreButton logic

--- src/main.js
+++ src/main.js
@@ -18,8 +18,15 @@ import {TaskEdit} from './components/task-edit';
 import {button} from './components/button';
 import {generateButtonTemplate} from './components/button';
 
+import {generateNoTasksTemplate} from './components/no-tasks';
+
 
 const MAX_CARDS_ON_BOARD = 8;
+
+const renderNoTasksMessage = (container) => {
+  container.innerHTML = ``;
+  renderComponent(generateNoTasksTemplate(), container);
+};
 
 const renderTask = (taskMock, container) => {
   const task = new Task(taskMock);
@@ -38,9 +45,13 @@ const renderTask = (taskMock, container) => {
       loadMoreButton.classList.add(`visually-hidden`);
     }
 
-    if (tasks.length >= MAX_CARDS_ON_BOARD) {
+    if (tasks.length >= document.querySelector(`.board__tasks`).childNodes.length) {
       const nextTaskIndex = parseInt(document.querySelector(`.board__tasks`).lastChild.getAttribute(`data-index`), 10) + 1;
       renderTask(tasks.find((item) => item.id === nextTaskIndex), container);
+    }
+
+    if (tasks.length === 0 || tasks.every((item) => item.isArchive)) {
+      renderNoTasksMessage(boardContainer);
     }
 
     taskEdit.removeElement();
@@ -95,17 +106,20 @@ const boardContainer = renderContainer(`section`, [`board`, `container`], mainCo
 // Tasks container
 const tasksContainer = renderContainer(`div`, [`board__tasks`], boardContainer);
 
+// Controls, search, filters
 renderComponent(generateConrolsTemplate(controls), controlsContainer);
 renderComponent(generateSearchTemplate(search), searchContainer);
 renderComponent(generateFiltersTemplate(filterNames), filtersContainer);
 
+// Tasks
 for (const task of tasks.slice(0, MAX_CARDS_ON_BOARD)) {
   renderTask(task, tasksContainer);
 }
 
+// Load more button
 renderComponent(generateButtonTemplate(button), boardContainer);
-
 const loadMoreButton = document.querySelector(`.load-more`);
+
 const onLoadMoreButtonClick = () => {
   for (const task of tasks.slice(MAX_CARDS_ON_BOARD)) {
     renderTask(task, tasksContainer);
@@ -115,3 +129,9 @@ const onLoadMoreButtonClick = () => {
 };
 
 loadMoreButton.addEventListener(`click`, onLoadMoreButtonClick);
+
+// No tasks message
+if (tasks.length === 0 || tasks.every((item) => item.isArchive)) {
+  renderNoTasksMessage(boardContainer);
+}
+
